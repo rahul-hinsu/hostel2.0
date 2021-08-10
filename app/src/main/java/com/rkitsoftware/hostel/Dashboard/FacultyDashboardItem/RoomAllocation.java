8b63c794e7e897a268cd0e5f5ad2477b2c73f7f4
rahul-hinsu <rahulhinsu9687@gmail.com>
2021-08-11 02:41:56 +0530

    Get student list

--- app/src/main/java/com/rkitsoftware/hostel/Dashboard/FacultyDashboardItem/RoomAllocation.java
+++ app/src/main/java/com/rkitsoftware/hostel/Dashboard/FacultyDashboardItem/RoomAllocation.java
@@ -1,12 +1,29 @@
 package com.rkitsoftware.hostel.Dashboard.FacultyDashboardItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rkitsoftware.hostel.Model.SignupModel;
 import com.rkitsoftware.hostel.R;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class RoomAllocation extends AppCompatActivity {
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+    SignupModel[] stu2 = new SignupModel[100];
 
     int numRoom;
     int bedPerRoom;
@@ -20,6 +37,28 @@ public class RoomAllocation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_allocation);
+
+        rootNode = FirebaseDatabase.getInstance();
+        reference = rootNode.getReference("signup");
+
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int i = 0;
+                for(DataSnapshot ds: snapshot.getChildren()){
+                    SignupModel tmp;
+                    tmp = ds.getValue(SignupModel.class);
+                    stu2[i] = tmp;
+                    i++;
+                }
+                Log.d("h","----------------"+ stu2[3].getName());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         totalSeats = numRoom * bedPerRoom;
         scSeats = (int)(totalSeats * 0.15);
