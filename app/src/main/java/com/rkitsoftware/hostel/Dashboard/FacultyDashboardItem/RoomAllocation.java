c800c51305cfb55971db78cf82c06ffa45ab5ae1
rahul-hinsu <rahulhinsu9687@gmail.com>
2021-08-10 23:03:20 +0530

    Variable declairation

--- app/src/main/java/com/rkitsoftware/hostel/Dashboard/FacultyDashboardItem/RoomAllocation.java
+++ app/src/main/java/com/rkitsoftware/hostel/Dashboard/FacultyDashboardItem/RoomAllocation.java
@@ -8,9 +8,28 @@ import com.rkitsoftware.hostel.R;
 
 public class RoomAllocation extends AppCompatActivity {
 
+    int numRoom;
+    int bedPerRoom;
+    int totalSeats;
+    int scSeats;
+    int stSeats;
+    int obcSeats;
+    int openSeats;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_allocation);
+
+        totalSeats = numRoom * bedPerRoom;
+        scSeats = (int)(totalSeats * 0.15);
+        stSeats = (int)(totalSeats * 0.075);
+        obcSeats = (int)(totalSeats * 0.27);
+        openSeats = totalSeats - scSeats - stSeats - obcSeats;
+        shortStudent();
+    }
+
+    void shortStudent(){
+
     }
 }
